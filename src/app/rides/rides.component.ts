1406f691fba92d4b510c920dc95a8035c1871c1f
Jacob Good <jacobgood97@gmail.com>
2018-04-03 06:59:40 -0400

    trying to create a google map on rides

--- src/app/rides/rides.component.ts
+++ src/app/rides/rides.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from "@angular/router";
 import { Observable } from 'rxjs/Observable';
 import { tap, catchError } from 'rxjs/operators';
 import { of } from 'rxjs/observable/of';
+import { } from '@types/googlemaps';
 
 @Component({
   selector: 'app-rides',
@@ -11,10 +12,22 @@ import { of } from 'rxjs/observable/of';
   styleUrls: ['./rides.component.css']
 })
 export class RidesComponent implements OnInit {
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
 
   constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
+    var mapProp = {
+      center: new google.maps.LatLng(15.5893, 73.8143),
+      zoom: 15,
+      mapTypeIdL google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
   }
+
+  setMapType(mapTypeId: string) {
+    this.map.setMapTypeId(mapTypeId)    
+}
 
 }
